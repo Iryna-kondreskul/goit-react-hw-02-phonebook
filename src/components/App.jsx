ef28eb5a509bccf04c1249492bb1893985bc03d3
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-07-25 03:52:03 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,57 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import css from './App.module.css'
+ 
 class App extends Component {
   
   state = {
     contacts: [],
-    name: '',
+    name:'',
   };
 
+  addContact = (data) => {
+    const newContact = {
+     ...data,
+    id:nanoid(),
+  }
+    console.log(newContact);
+
+ this.state.contacts.find(contact => contact.name === data.name)
+ ? alert('Контакт з таким ім\'ям уже існує')
+ : this.setState(({ contacts }) => ({
+  contacts: [newContact, ...contacts],
+}));
+  console.log(data)
+   }
+
+   deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+
+
   render () {
+    
     return (
-      <div>
-      <h1>Phonebook</h1>
-      {/* <ContactForm ... /> */}
-  
-      <h2>Contacts</h2>
-      {/* <Filter ... /> */}
-      {/* <ContactList ... /> */}
+      <div className={css.container}>
+        
+      <h1 className={css.title}>Phone
+      <span className={css.spanColor}>book</span>
+      </h1>
+
+      <ContactForm onSubmit={this.addContact} />
+
+      <h2 className={css.header}>Contacts</h2>
+
+      {/* <Filter /> */}
+      <ContactList 
+      contacts={this.state.contacts} 
+      onDeleteContact={this.deleteContact}/>
+
       </div>
     );
   }
